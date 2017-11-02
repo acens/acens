9d8eb40302109cf3e811c1b9b3bd2badc42d7bf6
Gleidson Silva <gleidson.fgs@gmail.com>
2017-11-02 17:05:34 -0300

    alterando tipo da coluna

--- db/migrate/20171023005047_add_attributes_to_users.rb
+++ db/migrate/20171023005047_add_attributes_to_users.rb
@@ -1,7 +1,7 @@
 class AddAttributesToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :name, :string
-    add_column :users, :picture, :attachment
+    add_column :users, :picture, :string
     add_column :users, :position_held, :string
     add_column :users, :description, :text
     add_reference :users, :user_contact, foreign_key: true
