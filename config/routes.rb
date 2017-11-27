fa12bb119aae21c705cb522104462c38bc8be0d1
Gleidson Silva <gleidson.fgs@gmail.com>
2017-11-27 12:22:33 -0300

    Rota

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :portfolios
   resources :surveys, only: [:new, :create]
-  get "/pesquisa", :to => "surveys#new"
+  get "/inscricao", :to => "surveys#new"
   root "home#index"
 
   resources "contacts", only: [:new, :create]
